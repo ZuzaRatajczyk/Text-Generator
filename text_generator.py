a8d0dd0bb120aa4eca9530b0c04511c2382d2d5a
Zuza <zuza.ratajczyk@gmail.com>
2021-05-29 16:23:31 +0200

    2 stage of project

--- text_generator.py
+++ text_generator.py
@@ -6,21 +6,26 @@ def main():
     tk = WhitespaceTokenizer()
     corpus_file = open(input(), 'r', encoding='utf-8')
     list_of_tokens = tk.tokenize(corpus_file.read())
-    print('Corpus statistics')
-    print(len(list_of_tokens))
-    print(len(set(list_of_tokens)))
+    list_of_bigrams = []
+    for i, token in enumerate(list_of_tokens):
+        if i == len(list_of_tokens)-1:
+            pass
+        else:
+            bigram = [token, list_of_tokens[i+1]]
+            list_of_bigrams.append(bigram)
+
+    print(f'Number of bigrams: {len(list_of_bigrams)}')
     while True:
         try:
             idx = input()
             if idx == 'exit':
                 break
             else:
-                print(list_of_tokens[int(idx)])
+                print(f'Head: {list_of_bigrams[int(idx)][0]}    Tail: {list_of_bigrams[int(idx)][1]}')
         except IndexError:
             print('Index Error. Please input an integer that is in the range of the corpus.')
         except ValueError:
             print('Type Error. Please input an integer.')
-
 
 
 if __name__ == "__main__":
