748b1de540cdbe6e57df9d6d0497e7767aaa7aab
Zuza <zuza.ratajczyk@gmail.com>
2021-06-02 14:40:44 +0200

    5 Stage of project

--- text_generator.py
+++ text_generator.py
@@ -2,14 +2,40 @@ from nltk.tokenize import WhitespaceTokenizer
 from collections import Counter
 import random
 
+PUNCTUATION_MARKS = '.?!'
+
 
 def find_next_word(word, dict_of_freq):
     return random.choices(list(dict_of_freq[word].keys()), weights=list(dict_of_freq[word].values()))[0]
 
 
+def generate_first_word(dict_of_freq):
+    while True:
+        word = random.choice(list(dict_of_freq.keys()))
+        if word[0].isupper() and word[-1] not in PUNCTUATION_MARKS:
+            return word
+
+
+def generate_sentence(dict_of_freq):
+    curr_word = generate_first_word(dict_of_freq)
+    for curr_word_idx in range(5):
+        if curr_word[-1] in PUNCTUATION_MARKS:
+            print(curr_word[:-1], end=' ')
+        else:
+            print(curr_word, end=' ')
+        curr_word = find_next_word(curr_word, dict_of_freq)
+
+    while True:
+        curr_word = find_next_word(curr_word, dict_of_freq)
+        if curr_word[-1] in PUNCTUATION_MARKS:
+            print(curr_word, end='\n')
+            break
+        else:
+            print(curr_word, end=' ')
+
+
 def main():
     tk = WhitespaceTokenizer()
-    # print("Enter corpus file:")
     corpus_file = open(input(), 'r', encoding='utf-8')
     list_of_tokens = tk.tokenize(corpus_file.read())
     dict_of_bigrams = {}
@@ -27,13 +53,7 @@ def main():
         dict_of_bigrams[key] = frq_counter
 
     for curr_sentence_idx in range(10):
-        curr_word = random.choice(list(dict_of_bigrams.keys()))
-        for curr_word_idx in range(10):
-            if curr_word_idx == 9:
-                print(f'{curr_word}')
-            else:
-                print(f'{curr_word} ', end='')
-            curr_word = find_next_word(curr_word, dict_of_bigrams)
+        generate_sentence(dict_of_bigrams)
 
 
 if __name__ == "__main__":
